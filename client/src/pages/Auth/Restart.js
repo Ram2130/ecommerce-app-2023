9a6b8c2198a1440729b8bcc4e085ceb49d653b8e
Ram prajapat <gajanandprajapat843@gmail.com>
2024-05-14 19:14:20 +0530

    done

---
+++ client/src/pages/Auth/Restart.js
@@ -0,0 +1,49 @@
+import React, { useState,useContext } from "react";
+import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import { useNavigate, useLocation } from "react-router-dom";
+import toast from "react-hot-toast";
+import "../../styles/AuthStyles.css";
+import {AuthProvider} from "../../context/auth";
+
+const Restart =() => {
+    
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   //const [auth, setAuth] = useContext(AuthProvider); 
+
+  // const navigate = useNavigate();
+  // const location = useLocation();
+
+  // // form function
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const res = await axios.post("/api/v1/auth/login", {
+  //       email,
+  //       password,
+  //     });
+  //     if (res && res.data.success) {
+  //       toast.success(res.data && res.data.message);
+  //       setAuth({
+  //         ...auth,
+  //         user: res.data.user,
+  //         token: res.data.token,
+  //       });
+  //       localStorage.setItem("auth", JSON.stringify(res.data));
+  //       navigate(location.state || "/");
+  //     } else {
+  //       toast.error(res.data.message);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     toast.error("Something went wrong");
+  //   }
+ // };
+  return (<Layout title="Register - Ecommer App">
+    <div>log</div>
+    </Layout>
+  )
+}
+
+export default Restart
